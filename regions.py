f2374e3e955bd45649ba7a74eb92e790cf3307ce
Jorge Morales <jamorales516@icloud.com>
2021-05-22 12:24:47 -0500

    getting regions

---
+++ regions.py
@@ -0,0 +1,28 @@
+import requests
+import hashlib
+import sqlite3
+import json
+import pandas
+
+
+url = "https://restcountries-v1.p.rapidapi.com/all"
+
+headers = {
+    'x-rapidapi-key': "7c4f3a3762msh144ae6194839930p18a12ajsndd1c892fcda9",
+    'x-rapidapi-host': "restcountries-v1.p.rapidapi.com"
+    }
+
+response = requests.request("GET", url, headers=headers)
+
+time1= response.elapsed.total_seconds()
+
+regions = []
+
+my_regions = {}
+regions_dt = {'region':[], 'country':[], 'language':[], 'time':[]}
+
+for r in response.json():
+    region = r['region']
+    if region not in regions and region != "":
+        regions.append(region)
+
