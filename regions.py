a345a892c0057f2bfa7f2464d76b252c936ef22b
Jorge Morales <jamorales516@icloud.com>
2021-05-22 12:28:19 -0500

    def created to get regions

--- regions.py
+++ regions.py
@@ -5,24 +5,32 @@ import json
 import pandas
 
 
-url = "https://restcountries-v1.p.rapidapi.com/all"
+def get_regions():
+    url = "https://restcountries-v1.p.rapidapi.com/all"
 
-headers = {
-    'x-rapidapi-key': "7c4f3a3762msh144ae6194839930p18a12ajsndd1c892fcda9",
-    'x-rapidapi-host': "restcountries-v1.p.rapidapi.com"
-    }
+    headers = {
+        'x-rapidapi-key': "7c4f3a3762msh144ae6194839930p18a12ajsndd1c892fcda9",
+        'x-rapidapi-host': "restcountries-v1.p.rapidapi.com"
+        }
 
-response = requests.request("GET", url, headers=headers)
+    response = requests.request("GET", url, headers=headers)
 
-time1= response.elapsed.total_seconds()
+    time1= response.elapsed.total_seconds()
 
-regions = []
+    regions = []
 
-my_regions = {}
-regions_dt = {'region':[], 'country':[], 'language':[], 'time':[]}
+    for r in response.json():
+        region = r['region']
+        if region not in regions and region != "":
+            regions.append(region)
+    
+    return regions, time1
 
-for r in response.json():
-    region = r['region']
-    if region not in regions and region != "":
-        regions.append(region)
+
+if __name__ == "__main__":
+    regions = []
+    my_regions = {}
+    regions_dt = {'region':[], 'country':[], 'language':[], 'time':[]}
+
+    regions, time1= get_regions()
 
